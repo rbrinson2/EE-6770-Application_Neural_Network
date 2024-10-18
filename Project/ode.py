5280775e6d08920c949258bf45f0aa1557202f5f
rbrin <rbrinson2@gmail.com>
2024-10-18 15:52:12 -0400

    Added some files

---
+++ Project/ode.py
@@ -0,0 +1,10 @@
+import tensorflow as tf
+from tensorflow import keras
+
+from keras.api.layers import Dense
+from keras.api.models import Sequential
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import math
